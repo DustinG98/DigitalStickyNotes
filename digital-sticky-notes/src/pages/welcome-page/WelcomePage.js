d7dcea3f891438fc0b8352bedeb2f0edf22d9108
DustinG98 <thedustingraham@gmail.com>
2019-11-17 17:32:16 -0700

    Setup routing and added a welcome page and notes page

---
+++ digital-sticky-notes/src/pages/welcome-page/WelcomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WelcomePage = () => {
+    return (
+        <div>
+            <h1>Welcome To Digital Sticky Notes!</h1>
+        </div>
+    )
+}
+
+export default WelcomePage
